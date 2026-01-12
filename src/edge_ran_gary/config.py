816b84fafdf45642cccf2fb2bef83a6bcb708c3b
Ananya-Jha-code <ananya.a.jha123@gmail.com>
2026-01-12 09:39:22 -0500

    loading script+config

--- src/edge_ran_gary/config.py
+++ src/edge_ran_gary/config.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 @dataclass
-class TrainingConfig:
-    seed: int = 42
-    batch_size: int = 256
-    epochs: int = 50
-    learning_rate: float = 1e-3
+class SpectrumXDatasetConfig:
+    dataset_root: Path = Path("competition_dataset")
+    sds_host: str = "sds.crc.nd.edu"
+
+
+# @dataclass
+# class TrainingConfig:
+#     seed: int = 42
+#     batch_size: int = 256
+#     epochs: int = 50
+#     learning_rate: float = 1e-3
 
